04d4406f8db0e7414869b99326d81c073735ed9b
PugazharasanC <32019083+PugazharasanC@users.noreply.github.com>
2020-06-02 18:00:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let URL = 'http://api.qrserver.com/v1/create-qr-code/?ecc=H&';
+
+let getMyQR = async() => {
+    let data = document.getElementById('data').value;
+    let widthVal = parseInt(document.getElementById('size1').value);
+    let heightVal = parseInt(document.getElementById('size2').value);
+    let color = document.getElementById('favcolor').value;
+    let bgcolor = document.getElementById('bgcolor').value;
+    let format = document.getElementById('format').value;
+    let myDiv = document.getElementById('imgDiv');
+    let imageLink = URL + 'data=' + data + '&' + widthVal + '*' + heightVal + '&color=' + color.replace('#', '').toUpperCase() + '&bgcolor=' + bgcolor.replace('#', '').toUpperCase() + '&format=' + format;
+    let image = document.createElement('img');
+    image.src = imageLink;
+    image.alt = data;
+    image.id = 'myImage';
+    image.width = widthVal;
+    image.height = heightVal;
+    if (myDiv.childNodes.length > 0) {
+        myDiv.childNodes[0].remove();
+        myDiv.appendChild(image)
+    } else {
+        myDiv.appendChild(image);
+    }
+}
